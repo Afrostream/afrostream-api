61db5b1e0e3be7c77c1f761a716123a4b90a9c2f
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-07-05 10:28:38 +0200

    feature: graphql example

--- main.go
+++ main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/afrostream/afrostream-go-lib/server"
 	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/graphql"
 )
-
-func Index(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("Afrostream API V2"))
-}
 
 func main() {
 	var s *server.Server
 
 	s = server.New()
-	s.Engine.GET("/hello-world", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello, World!")
+
+	// graphql
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	s.Engine.GET("/", func(c *gin.Context) {
+		query := c.Query("query")
+		params := graphql.Params{Schema: schema, RequestString: query}
+		r := graphql.Do(params)
+		if len(r.Errors) > 0 {
+			log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+		}
+		rJSON, _ := json.Marshal(r)
+		c.String(http.StatusOK, fmt.Sprintf("%s", rJSON))
 	})
 	s.Spawn(CONF_DEFAULT_PORT)
 }
